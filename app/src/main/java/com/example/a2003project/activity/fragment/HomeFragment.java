f543cc518ee07cd0624f28152c261b5c01eb17c3
HongYang456 <2395909838@qq.com>
2020-12-25 19:45:31 +0800

    第二次提交

--- app/src/main/java/com/example/a2003project/activity/fragment/HomeFragment.java
+++ app/src/main/java/com/example/a2003project/activity/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.a2003project.activity.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,8 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.alibaba.android.vlayout.DelegateAdapter;
 import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.alibaba.android.vlayout.layout.ColumnLayoutHelper;
+import com.alibaba.android.vlayout.layout.GridLayoutHelper;
 import com.example.a2003project.R;
+import com.example.a2003project.activity.adapter.ColumnAdapter;
 import com.example.a2003project.activity.utils.BannerLoader;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
@@ -28,6 +33,7 @@ public class HomeFragment extends Fragment {
     private String BannerUrl1 = "http://yanxuan.nosdn.127.net/65091eebc48899298171c2eb6696fe27.jpg";
     private String BannerUrl2 = "http://yanxuan.nosdn.127.net/bff2e49136fcef1fd829f5036e07f116.jpg";
     private String BannerUrl3 = "http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg";
+    private VirtualLayoutManager virtualLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,13 +52,43 @@ public class HomeFragment extends Fragment {
 
         // 创建VirtualLayoutManager对象
         // 同时内部会创建一个LayoutHelperFinder对象，用来后续的LayoutHelper查找
-        VirtualLayoutManager virtualLayoutManager = new VirtualLayoutManager(getContext());
+        virtualLayoutManager = new VirtualLayoutManager(getContext());
         // 将VirtualLayoutManager绑定到recyclerView
         recycler.setLayoutManager(virtualLayoutManager);
         // 设置组件复用回收池
         RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
         recycler.setRecycledViewPool(viewPool);
         viewPool.setMaxRecycledViews(0, 10);
+        //栏格布局
+        GridLayout();
+    }
+
+    private void GridLayout(){
+        /**
+         设置Grid布局
+         */
+        GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(1);
+        // 在构造函数设置每行的网格个数
+        // 公共属性
+        gridLayoutHelper.setItemCount(5);// 设置布局里Item个数
+        gridLayoutHelper.setPadding(20, 20, 20, 20);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
+        gridLayoutHelper.setMargin(20, 20, 20, 20);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+        gridLayoutHelper.setBgColor(Color.WHITE);// 设置背景颜色
+        gridLayoutHelper.setAspectRatio(6);// 设置设置布局内每行布局的宽与高的比
+// gridLayoutHelper特有属性（下面会详细说明）
+        gridLayoutHelper.setWeights(new float[]{20, 20, 20, 20, 20});//设置每行中 每个网格宽度 占 每行总宽度 的比例
+        gridLayoutHelper.setVGap(20);// 控制子元素之间的垂直间距
+        gridLayoutHelper.setHGap(20);// 控制子元素之间的水平间距
+        gridLayoutHelper.setAutoExpand(false);//是否自动填充空白区域
+        gridLayoutHelper.setSpanCount(5);// 设置每行多少个网格
+        ColumnAdapter mainGridAdapter = new ColumnAdapter(gridLayoutHelper,getContext());
+        DelegateAdapter adapter = new DelegateAdapter(virtualLayoutManager,true);
+
+        adapter.addAdapter(mainGridAdapter);
+
+        recycler.setLayoutManager(virtualLayoutManager);
+
+        recycler.setAdapter(adapter);
     }
 
     private void BannerData() {
